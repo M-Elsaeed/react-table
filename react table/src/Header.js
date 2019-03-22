c8e97bd3f7fba2fd792daf41948f2f4f9e1bbdc5
M-Elsaeed <m.elsaeed1998@gmail.com>
2019-03-22 17:16:10 +0200

    Deleting unneccesary files

--- react table/src/Header.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react';
-class Header extends Component {
-    render() {
-        return (
-            <p>Header Here</p>
-        );
-    }
-}
-export default Header;
